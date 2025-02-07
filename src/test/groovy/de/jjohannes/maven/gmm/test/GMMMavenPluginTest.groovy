84cc80b71b54a9934a61bd00fdab4c05cccdb5b3
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-02-07 10:22:20 +0100

    Switch build setup to Gradle (#24)

--- src/test/groovy/de/jjohannes/maven/gmm/test/GMMMavenPluginTest.groovy
+++ src/test/groovy/de/jjohannes/maven/gmm/test/GMMMavenPluginTest.groovy
@@ -1,11 +1,11 @@
 /*
- * Copyright 2020 the original author or authors.
+ * Copyright the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.jjohannes.maven.gmm.integtests
+
+package de.jjohannes.maven.gmm.test
 
 import com.google.gson.JsonElement
 import com.google.gson.JsonParser
@@ -235,9 +236,9 @@ class GMMMavenPluginTest extends Specification {
     }
 
     private void assertExpectedGMM(String name) throws Exception  {
-        File testPom = new File("../src/test/resources/$name/pom.xml")
-        File testPomParent = new File("../src/test/resources/$name/parent/pom.xml")
-        File gmmExpected = new File("../src/test/resources/$name/expected-module.json")
+        File testPom = new File("src/test/resources/$name/pom.xml")
+        File testPomParent = new File("src/test/resources/$name/parent/pom.xml")
+        File gmmExpected = new File("src/test/resources/$name/expected-module.json")
         assertTrue(gmmExpected.exists())
 
         Files.copy(testPom.toPath(), mavenProducerBuild.toPath())
